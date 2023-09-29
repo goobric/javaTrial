b4a1e527a96ca6f7b12223ef2d77f4a6fab1085c
goobric <goobric@gmail.com>
2023-09-29 19:25:41 +0800

    math trig

---
+++ main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+  public static void main(String[] args){
+    // value a, b, c
+    double a = 2.3, b =4, c = 5.6;
+    double root1, root2;
+
+    // calculate the determinant (b2-4ac)
+    double determinant = b * b - 4 * a * c;
+
+    // check if the determinant is greater than zero 0
+    if (determinant > 0){
+      // two real and distinct roots
+      root1 = (-b + Math.sqrt(determinant)) / (2 * a);
+      root2 = (-b - Math.sqrt(determinant)) / (2 * a);
+      System.out.format("root1 = %.2f and root2 = %.2f", root1, root2);
+    }
+  }
+}
