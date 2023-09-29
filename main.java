493dc4553ca651206fc9a45d8acdd400634addba
goobric <goobric@gmail.com>
2023-09-29 19:28:56 +0800

    math trig2

--- main.java
+++ main.java
@@ -15,5 +15,13 @@ public class Main {
       root2 = (-b - Math.sqrt(determinant)) / (2 * a);
       System.out.format("root1 = %.2f and root2 = %.2f", root1, root2);
     }
+    // check if the determinant is equal to zero
+    else if (determinant == 0){
+      // two real and equal roots
+      //determinant is equal to 0
+      // so -b + 0 == -b
+      root1 = root2 = -b / (2 * a);
+      System.out.format("root1 = root2 = %.2f:", root1);
+    }
   }
 }
